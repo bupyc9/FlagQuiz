c755abbaf034f8082127a2d1ec7c3141fbe6ce3c
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-01-09 23:09:53 +0300

    Anonymous class "OnSharedPreferenceChangeListener"  in "MainActivity"

--- app/src/main/java/ru/bupyc9/flagquiz/MainActivity.java
+++ app/src/main/java/ru/bupyc9/flagquiz/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.bupyc9.flagquiz;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -9,12 +10,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     public static final String CHOICES = "pref_numberOfChoices";
     public static final String REGIONS = "pref_regionsToInclude";
     private boolean phoneDevice = true;
     private boolean preferenceChange = true;
+    private SharedPreferences.OnSharedPreferenceChangeListener preferenceChangeListListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        // Вызывается при изменение настроек приложения
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            preferenceChange = true; // Пользователь изменил настройки
+            MainActivityFragment quizFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+            if (key.equals(CHOICES)) { // Изменилось число вариантов
+                quizFragment.updateGuessRows(sharedPreferences);
+                quizFragment.resetQuiz();
+            } else if (key.equals(REGIONS)) { // Изменились регионы
+                Set<String> regions = sharedPreferences.getStringSet(REGIONS, null);
+                if (regions != null && regions.size() > 0) {
+                    quizFragment.updateRegions(sharedPreferences);
+                    quizFragment.resetQuiz();
+                } else { // Хотя бы один регион - по умолчанию Северная Америка
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    regions.add(getString(R.string.default_region));
+                    editor.putStringSet(REGIONS, regions);
+                    editor.apply();
+                    Toast.makeText(MainActivity.this, R.string.default_region_message, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            Toast.makeText(MainActivity.this, R.string.restarting_quiz, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         // Регистрация слушателя для изменений SharedPreferences
         PreferenceManager
                 .getDefaultSharedPreferences(this)
-                .registerOnSharedPreferenceChangeListener(preferenceChangeListListener);
+                .registerOnSharedPreferenceChangeListener(this.preferenceChangeListListener);
         // Определение размера экрана
         int screenSize = this.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
         // Для планшетного устройства phoneDevice присваивается false

88fdf9a2d610f0f0a8caca4e2e21fb6311d87d0f
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-01-09 22:45:57 +0300

    Override "onOptionsItemSelected" in "MainActivity"

--- app/src/main/java/ru/bupyc9/flagquiz/MainActivity.java
+++ app/src/main/java/ru/bupyc9/flagquiz/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.bupyc9.flagquiz;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -73,15 +74,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        Intent preferenceIntent = new Intent(this, SettingsActivity.class);
+        this.startActivity(preferenceIntent);
 
         return super.onOptionsItemSelected(item);
     }

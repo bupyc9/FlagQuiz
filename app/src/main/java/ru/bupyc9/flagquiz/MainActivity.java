c5d1e9df1ae8f9a371be6aa812409276e2368fd3
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-01-09 22:36:11 +0300

    Override "onCreate" in "MainActivity"

--- app/src/main/java/ru/bupyc9/flagquiz/MainActivity.java
+++ app/src/main/java/ru/bupyc9/flagquiz/MainActivity.java
@@ -1,12 +1,19 @@
 package ru.bupyc9.flagquiz;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGIONS = "pref_regionsToInclude";
+    private boolean phoneDevice = true;
+    private boolean preferenceChange = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // Задание значений по умолчанию в файле SharedPreferences
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        // Регистрация слушателя для изменений SharedPreferences
+        PreferenceManager
+                .getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(preferenceChangeListListener);
+        // Определение размера экрана
+        int screenSize = getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+        // Для планшетного устройства phoneDevice присваивается false
+        if (
+            screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE
+            || screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE
+        ) {
+            this.phoneDevice = false; // Не соответствует размерам телефона
+        }
+
+        // На телефоне разрешена только портретная ориентация
+        if (this.phoneDevice) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
     }
 
     @Override

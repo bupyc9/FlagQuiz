730b544b7bd7d640bcdab9f007c1d02d9c3b9ec2
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-01-13 00:18:55 +0300

    Creating a GUI configuration file for preferences.xml

--- app/src/main/java/ru/bupyc9/flagquiz/SettingsActivityFragment.java
+++ app/src/main/java/ru/bupyc9/flagquiz/SettingsActivityFragment.java
@@ -1,22 +1,13 @@
 package ru.bupyc9.flagquiz;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class SettingsActivityFragment extends Fragment {
-
-    public SettingsActivityFragment() {
-    }
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreate(@Nullable Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
